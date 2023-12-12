e8cc8642dba1b5f67706b68a0d2e4829d95a1f1d
MA. LOURDES SHAINE RAZO <2230574@slu.edu.ph>
2023-12-12 15:55:40 +0000

    Update file Distance.java

--- src/finlab/Distance.java
+++ src/finlab/Distance.java
@@ -1,4 +1,20 @@
 package finlab;
 
+package finlab;
 public class Distance {
+    private final Vertex graphNode;
+    private final double distanceValue;
+
+    public Distance(Vertex node, double distance) {
+        this.graphNode = node;
+        this.distanceValue = distance;
+    }
+
+    public Vertex getGraphNode() {
+        return graphNode;
+    }
+
+    public double getDistanceValue() {
+        return distanceValue;
+    }
 }
