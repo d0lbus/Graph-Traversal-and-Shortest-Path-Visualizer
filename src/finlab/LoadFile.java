bd52c4c5baa2119a33aa4c75bee4154bc6c8fd61
Chloe San Miguel <2232501@slu.edu.ph>
2023-12-13 12:37:53 +0800

    changes for shortest path

--- src/finlab/LoadFile.java
+++ src/finlab/LoadFile.java
@@ -98,13 +98,11 @@ public class LoadFile {
      */
     public static void displayGraph(finlab.Graph graph) {
         if (graph != null && !graph.getVertices().isEmpty()) {
-            JFrame frame = new JFrame("Graph Display");
+            JFrame frame = new JFrame("Graph Illustration");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800, 600);
-
+            frame.setSize(500, 300);
             LoadFile.GraphPanel graphPanel = new GraphPanel(graph);
             frame.add(graphPanel);
-
             frame.setVisible(true);
             frame.setLocationRelativeTo(null);
 
@@ -143,16 +141,12 @@ public class LoadFile {
 
             graph.getVertexById("A").setXPos(centerX - spacingX);
             graph.getVertexById("A").setYPos(centerY - spacingY);
-
             graph.getVertexById("B").setXPos(centerX + spacingX);
             graph.getVertexById("B").setYPos(centerY - spacingY);
-
             graph.getVertexById("C").setXPos(centerX - spacingX);
             graph.getVertexById("C").setYPos(centerY + spacingY);
-
             graph.getVertexById("D").setXPos(centerX + spacingX);
             graph.getVertexById("D").setYPos(centerY + spacingY);
-
             graph.getVertexById("E").setXPos(centerX);
             graph.getVertexById("E").setYPos(centerY + 2 * spacingY);
         }
@@ -171,12 +165,10 @@ public class LoadFile {
             for (Vertex node : graph.getVertices()) {
                 for (finlab.Edge edge : node.getConnectedEdges()) {
                     Vertex adjacentNode = edge.getDestination();
-
                     g.drawLine(node.getXPos(), node.getYPos(), adjacentNode.getXPos(), adjacentNode.getYPos());
-
                     int weightX = (node.getXPos() + adjacentNode.getXPos()) / 2;
                     int weightY = (node.getYPos() + adjacentNode.getYPos()) / 2;
-                    g.setColor(Color.BLUE);
+                    g.setColor(Color.RED);
                     g.drawString(String.valueOf(edge.getWeight()), weightX, weightY);
                     g.setColor(Color.BLACK);
                 }
@@ -186,7 +178,7 @@ public class LoadFile {
 
             for (Vertex node : graph.getVertices()) {
                 g.fillOval(node.getXPos() - 15, node.getYPos() - 15, 30, 30);
-                g.setColor(Color.WHITE);
+                g.setColor(Color.YELLOW);
                 g.drawString(node.getVertexId(), node.getXPos() - 5, node.getYPos() + 5);
                 g.setColor(Color.BLACK);
             }
